49ca1fe7b284cb8085c91998bfe5d0e11e3ad8fb
Genário Carneiro <53302984+genariocoliveira@users.noreply.github.com>
2023-12-25 18:17:17 +0000

    Update dez 2023

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-yellow-600 shadow-lg mt-5 text-center">
+    <footer className="bg-yellow-600 shadow-lg mt-5 text-center py-6">
       <p className="font-bold text-gray-700">
         © Genário Carneiro. Todos os direitos reservados.
       </p>
