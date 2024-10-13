2fdf19af578610f638494346ae26def2e8a9e8b4
Genário Carneiro <53302984+genariocoliveira@users.noreply.github.com>
2024-10-13 10:30:31 -0300

    Atualização, implantação da bilbioteca format number

--- src/GeradorTexto.js
+++ src/GeradorTexto.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import React, { useState, useRef } from "react";
+import { PatternFormat } from 'react-number-format';
 
 const GeradorTexto = () => {
   const [nomeMotorista, setNomeMotorista] = useState("");
@@ -57,18 +58,18 @@ const GeradorTexto = () => {
       setDataViagem("");
       setSolucao("");
     };
-    const textoGerado = `Informo que o carro *${numeroCarro}*, horário *${horarioCarro}*, linha *${linhaCarro.toUpperCase()}*, do dia *${dataViagem}*, foi socorrido com o carro *${numeroSocorro}*. Segue abaixo as informações.
-    - Motorista: *${nomeMotorista.toUpperCase()}*
-    - Plantão CCO: *${plantaoCco.toUpperCase()}*
-    - Horário do Chamado: *${horarioChamado}*
-    - Horário da Saída do Socorro: *${horarioSaidaSocorro}*
-    - Chegado no Local: *${chegadaLocal.toUpperCase()}*
-    - Motivo do SOS: *${motivoSOS.toUpperCase()}*
-    - Local: *${localSOS.toUpperCase()}*
-    - Houve Troca do Carro? *${houveTrocaCarro.toUpperCase()}*
-    - Horário de Retorno: *${horarioRetorno}*
-    - Horário de Chegada em Iguatu: *${horarioChegadaIguatu}*
-    - Solução: *${solucao}*`;
+    const textoGerado = `Informo que o carro ${numeroCarro}, horário ${horarioCarro}, linha ${linhaCarro.toUpperCase()}, do dia ${dataViagem}, foi socorrido com o carro ${numeroSocorro}. Segue abaixo as informações.
+- Motorista: ${nomeMotorista.toUpperCase()}
+- Plantão CCO: ${plantaoCco.toUpperCase()}
+- Horário do Chamado: ${horarioChamado}
+- Horário da Saída do Socorro: ${horarioSaidaSocorro}
+- Chegado no Local: ${chegadaLocal.toUpperCase()}
+- Motivo do SOS: ${motivoSOS.toUpperCase()}
+- Local: ${localSOS.toUpperCase()}
+- Houve Troca do Carro? ${houveTrocaCarro.toUpperCase()}
+- Horário de Retorno: ${horarioRetorno}
+- Horário de Chegada na Garagem: ${horarioChegadaIguatu}
+- Solução: ${solucao}`;
 
     setMensagemErro("");
     setTextoGerado(textoGerado);
@@ -90,55 +91,7 @@ const GeradorTexto = () => {
     }
   };
 
-  function formatarHorarioCarro() {
-    let horarioCar = document.querySelector("#horarioCarr");
-    let hCar = horarioCar.value;
-    if (hCar.length === 2) {
-      hCar = hCar + ":";
-      horarioCar.value = hCar; // Correção aqui
-    }
-  }
-  function formatarHorarioChamado() {
-    let horarioChamado = document.querySelector("#horarioChamado");
-    let chamado = horarioChamado.value;
-    if (chamado.length === 2) {
-      chamado = chamado + ":";
-      horarioChamado.value = chamado;
-    }
-  }
-  function formatarHorarioSaida() {
-    let saida = document.querySelector("#horaSaida");
-    let saidaH = saida.value;
-    if (saidaH.length === 2) {
-      saidaH = saidaH + ":";
-      saida.value = saidaH;
-    }
-  }
-  function formatarChegadaLocal() {
-    let chegadaLocal = document.querySelector("#chegadaLocal");
-    let local = chegadaLocal.value;
-    if (local.length === 2) {
-      local = local + ":";
-      chegadaLocal.value = local;
-    }
-  }
-  function formatarHorarioRetorno() {
-    let horarioRetorno = document.querySelector("#horarioRetorno");
-    let retorno = horarioRetorno.value;
-    if (retorno.length === 2) {
-      retorno = retorno + ":";
-      horarioRetorno.value = retorno;
-    }
-  }
-  function horarioChegadaIgu() {
-    let horaChegadaIAU = document.querySelector("#horaChegadaIgu"); // Adicione # antes de "horaChegadaIAU"
-    let HChegada = horaChegadaIAU.value;
-    if (HChegada.length === 2) {
-      HChegada = HChegada + ":";
-      horaChegadaIAU.value = HChegada;
-    }
-  }
-  function formatarDataViagem() {
+  /*function formatarDataViagem() {
     let viagem = document.querySelector("#dataViagem");
     let data = viagem.value;
     if (data.length === 2) {
@@ -149,10 +102,12 @@ const GeradorTexto = () => {
       data = data + "/";
       viagem.value = data;
     }
-  }
+  }*/
 
   return (
-    <div className="form break-words">
+    <div className="">
+      <div class="form break-words">
+        <h1 class="text-lg font-bold text-center uppercase">Informe quem esta solicitando SOS</h1>
       <label>
         Motorista:<spam className="text-red-500">*</spam>
         {mensagemErro && (
@@ -188,16 +143,12 @@ const GeradorTexto = () => {
           <p className="text-red-500 text-[.8rem]">{mensagemErro}</p>
         )}
       </label>
-      <input
-        id="horarioCarr"
-        className="input-type"
-        type="text"
-        placeholder="Informe o horário do carro"
+      <PatternFormat 
         value={horarioCarro}
-        onKeyPress={formatarHorarioCarro} // Correção aqui
-        onChange={(e) => setHorarioCarro(e.target.value)}
-        maxLength={5}
-      />
+        className="input-type" 
+        format="##:##" 
+        allowEmptyFormatting mask="_" 
+        onChange={(e) => setHorarioCarro(e.target.value)}/>
 
       <label>
         Linha do Carro:<spam className="text-red-500">*</spam>
@@ -219,173 +170,164 @@ const GeradorTexto = () => {
           <p className="text-red-500 text-[.8rem]">{mensagemErro}</p>
         )}
       </label>
-      <input
-        id="dataViagem"
-        className="input-type"
-        type="text"
-        placeholder="Informe a data da viagem"
-        value={dataViagem}
-        onKeyPress={formatarDataViagem}
-        onChange={(e) => setDataViagem(e.target.value)}
-        maxLength={10}
+      <PatternFormat
+      value={dataViagem}
+      className="input-type" 
+      format="##/##/####" 
+      allowEmptyFormatting mask="_" 
+      onChange={(e) => setDataViagem(e.target.value)}
       />
+      </div>
 
-      <label>
-        Nº do carro Socorro:<spam className="text-red-500">*</spam>
-      </label>
-      <input
-        className="input-type"
-        type="text"
-        placeholder="Nº do carro Socorro"
-        value={numeroSocorro}
-        onChange={(e) => setNumeroSocorro(e.target.value)}
-        maxLength={5}
-      />
+      <div class="form break-words my-4">
+        <h1 class="text-lg font-bold text-center uppercase">Informações do SOS</h1>
 
-      <label>
-        Horário do Chamado:<spam className="text-red-500">*</spam>
-      </label>
-      <input
-        id="horarioChamado"
-        className="input-type"
-        type="text"
+        <label>
+          Motivo do SOS:<spam className="text-red-500">*</spam>
+        </label>
+        <input
+          className="input-type"
+          type="text"
+          placeholder="Ex. Pneu furado"
+          value={motivoSOS}
+          onChange={(e) => setMotivoSOS(e.target.value)}
+        />
+
+        <label>
+          Local do Socorro:<spam className="text-red-500">*</spam>
+        </label>
+        <input
+          className="input-type"
+          type="text"
+          placeholder="Onde foi o socorro?"
+          value={localSOS}
+          onChange={(e) => setLocalSOS(e.target.value)}
+        />
+
+        <label>
+          Horário do Chamado:<spam className="text-red-500">*</spam>
+        </label>
+        <PatternFormat 
         value={horarioChamado}
-        placeholder="Horário do chamado"
-        onKeyPress={formatarHorarioChamado}
-        onChange={(e) => setHorarioChamado(e.target.value)}
-        maxLength={5}
-      />
+        className="input-type" 
+        format="##:##" 
+        allowEmptyFormatting mask="_" 
+        onChange={(e) => setHorarioChamado(e.target.value)}/>  
 
-      <label>
-        Horário da Saída do Socorro:<spam className="text-red-500">*</spam>
-      </label>
-      <input
-        id="horaSaida"
-        className="input-type"
-        type="text"
-        placeholder="Horário de Saída"
+        <label>
+          Nº do carro que saiu para o Socorro:<spam className="text-red-500">*</spam>
+        </label>
+        <input
+          className="input-type"
+          type="text"
+          placeholder="Nº do carro Socorro"
+          value={numeroSocorro}
+          onChange={(e) => setNumeroSocorro(e.target.value)}
+          maxLength={5}
+        />
+  
+        <label>
+          Horário da Saída do Socorro:<spam className="text-red-500">*</spam>
+        </label>
+        
+        <PatternFormat 
         value={horarioSaidaSocorro}
-        onKeyPress={formatarHorarioSaida}
-        onChange={(e) => setHorarioSaidaSocorro(e.target.value)}
-        maxLength={5}
-      />
-
-      <label>
-        Chegado no Local:<spam className="text-red-500">*</spam>
-      </label>
-      <input
-        id="chegadaLocal"
-        className="input-type"
-        type="text"
-        placeholder="Horário de chegada no local do SOS"
+        className="input-type" 
+        format="##:##" 
+        allowEmptyFormatting mask="_" 
+        onChange={(e) => setHorarioSaidaSocorro(e.target.value)}/>  
+          
+        <label>
+          Horário de Chegado no Local:<spam className="text-red-500">*</spam>
+        </label>
+        <PatternFormat 
         value={chegadaLocal}
-        onChange={(e) => setChegadaLocal(e.target.value)}
-        onKeyPress={formatarChegadaLocal}
-      />
+        className="input-type" 
+        format="##:##" 
+        allowEmptyFormatting mask="_" 
+        onChange={(e) => setChegadaLocal(e.target.value)}/>
+  
+        <label>
+          Houve Troca do Carro?<spam className="text-red-500">*</spam>
+        </label>
+        <input
+          className="input-type"
+          type="text"
+          placeholder="Sim / Não"
+          value={houveTrocaCarro}
+          onChange={(e) => setHouveTrocaCarro(e.target.value)}
+        />
+  
+        <label>
+          Horário de Retorno:<spam className="text-red-500">*</spam>
+        </label>
 
-      <label>
-        Motivo do SOS:<spam className="text-red-500">*</spam>
-      </label>
-      <input
-        className="input-type"
-        type="text"
-        placeholder="Ex. Pneu furado"
-        value={motivoSOS}
-        onChange={(e) => setMotivoSOS(e.target.value)}
-      />
-
-      <label>
-        Local do Socorro:<spam className="text-red-500">*</spam>
-      </label>
-      <input
-        className="input-type"
-        type="text"
-        placeholder="Onde foi o socorro?"
-        value={localSOS}
-        onChange={(e) => setLocalSOS(e.target.value)}
-      />
-
-      <label>
-        Houve Troca do Carro?<spam className="text-red-500">*</spam>
-      </label>
-      <input
-        className="input-type"
-        type="text"
-        placeholder="Sim / Não"
-        value={houveTrocaCarro}
-        onChange={(e) => setHouveTrocaCarro(e.target.value)}
-      />
-
-      <label>
-        Horário de Retorno:<spam className="text-red-500">*</spam>
-      </label>
-      <input
-        id="horarioRetorno"
-        className="input-type"
-        type="text"
-        placeholder="Digite o horário de retorno"
+        <PatternFormat 
         value={horarioRetorno}
-        onChange={(e) => setHorarioRetorno(e.target.value)}
-        onKeyPress={formatarHorarioRetorno}
-      />
+        className="input-type" 
+        format="##:##" 
+        allowEmptyFormatting mask="_" 
+        onChange={(e) => setHorarioRetorno(e.target.value)}/>
 
-      <label>
-        Horário de Chegada em Iguatu:<spam className="text-red-500">*</spam>
-      </label>
-      <input
-        id="horaChegadaIgu" // Adicione o ID aqui
-        className="input-type"
-        type="text"
-        placeholder="Informe o horário de chegada em Iguatu"
+        <label>
+          Horário de Chegada na Garagem:<spam className="text-red-500">*</spam>
+        </label>
+
+        <PatternFormat 
         value={horarioChegadaIguatu}
-        onKeyPress={horarioChegadaIgu} // Chame a função correta
-        onChange={(e) => setHorarioChegadaIguatu(e.target.value)}
-        maxLength={5}
-      />
+        className="input-type" 
+        format="##:##" 
+        allowEmptyFormatting mask="_" 
+        onChange={(e) => setHorarioChegadaIguatu(e.target.value)}/>
 
-      <label>
-        Plantão CCO:<spam className="text-red-500">*</spam>
-      </label>
-      <input
-        className="input-type"
-        type="text"
-        placeholder="Nome do plantonista"
-        value={plantaoCco}
-        onChange={(e) => setPlantaoCco(e.target.value)}
-      />
+        <label>
+          Plantão CCO:<spam className="text-red-500">*</spam>
+        </label>
+        <input
+          className="input-type"
+          type="text"
+          placeholder="Nome do plantonista"
+          value={plantaoCco}
+          onChange={(e) => setPlantaoCco(e.target.value)}
+        />
+  
+        <label>
+          Solução: <spam className="text-red-500">*</spam>
+        </label>
+        <textarea
+          value={solucao}
+          placeholder="Preencha a Solução do problema."
+          onChange={(e) => setSolucao(e.target.value)}
+        />
+      </div>
+      
+      <div class="form-res">
+        <button
+          onClick={gerarTexto}
+          className="uppercase bg-indigo-500 px-4 py-2 text-white font-medium rounded-lg hover:bg-indigo-700 duration-300 transition-colors shadow-md mb-4 w-full"
+        >
+          Gerar Texto
+        </button>
 
-      <label>
-        Solução: <spam className="text-red-500">*</spam>
-      </label>
-      <textarea
-        value={solucao}
-        placeholder="Preencha a Solução do problema."
-        onChange={(e) => setSolucao(e.target.value)}
-      />
+        {mensagemErro && <p className="text-red-500">{mensagemErro}</p>}
+  
+        {textoGerado && (
+          <div className="bg-white shadow-lg rounded-lg px-4 py-2">
+            <h3>Texto Gerado:</h3>
+            <pre className="my-4 whitespace-pre-wrap text-left" ref={textRef}>
+              {textoGerado}
+            </pre>
+            <button
+              className="uppercase bg-rose-500 px-4 py-2 text-white font-medium rounded-lg hover:bg-rose-900 duration-300 transition-colors shadow-md w-full"
+              onClick={handleCopyClick}
+            >
+              Copiar Texto
+            </button>
+          </div>
+        )}
+        
+      </div>
 
-      <button
-        onClick={gerarTexto}
-        className="uppercase bg-indigo-500 px-4 py-2 text-white font-medium rounded-lg hover:bg-indigo-700 duration-300 transition-colors shadow-md"
-      >
-        Gerar Texto
-      </button>
-
-      {mensagemErro && <p className="text-red-500">{mensagemErro}</p>}
-
-      {textoGerado && (
-        <div className="bg-white shadow-lg rounded-lg px-4 py-2">
-          <h3>Texto Gerado:</h3>
-          <pre className="my-4 whitespace-pre-wrap text-left" ref={textRef}>
-            {textoGerado}
-          </pre>
-          <button
-            className="uppercase bg-rose-500 px-4 py-2 text-white font-medium rounded-lg hover:bg-rose-900 duration-300 transition-colors shadow-md w-full"
-            onClick={handleCopyClick}
-          >
-            Copiar Texto
-          </button>
-        </div>
-      )}
     </div>
   );
 };
